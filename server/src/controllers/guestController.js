724e788c91063679e98235329f16d50a80441f5e
HamedPour <hamed_k.pour@hotmail.com>
2020-05-18 15:05:56 +0100

    Changes to database schema && db guest insertion

--- server/src/controllers/guestController.js
+++ server/src/controllers/guestController.js
@@ -4,11 +4,24 @@ module.exports = {
   async index(req, res) {
     let guests;
     try {
-      guests = await pool.query("SELECT * FROM guest");
+      guests = await pool.query(
+        "SELECT g.id, g.firstname, g.lastname, b.check_in, b.check_out FROM guest AS g INNER JOIN booking AS b ON b.id=g.id OR b.id!=g.id;"
+      );
     } catch (error) {
       res.status(500).send(error.message);
     }
-    // send the guest rows to client
     res.status(200).send(guests.rows);
+  },
+  async saveGuest(req, res) {
+    const { firstname, lastname } = req.body;
+    try {
+      await pool.query(
+        "INSERT INTO guest (firstname, lastname) VALUES ($1, $2)",
+        [firstname, lastname]
+      );
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+    res.status(200).send("New Guest Saved");
   },
 };
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,4 +2,5 @@ const guestController = require("./controllers/guestController");
 
 module.exports = (app) => {
   app.get("/guests", guestController.index);
+  app.post("/guests", guestController.saveGuest);
 };
